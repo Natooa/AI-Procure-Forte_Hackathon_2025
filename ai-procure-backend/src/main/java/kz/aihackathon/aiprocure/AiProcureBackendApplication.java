5b0372903b6688883da448c9c7eb23b02a9f6fc3
Nurdiyar <nurdi.instagramm@gmail.com>
2025-11-24 01:10:46 +0500

    refactor package system for more readable and comfort development and also create basic java spring project

---
+++ ai-procure-backend/src/main/java/kz/aihackathon/aiprocure/AiProcureBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.aihackathon.aiprocure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProcureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiProcureBackendApplication.class, args);
+    }
+
+}
---
+++ ai-procure-backend/src/test/java/kz/aihackathon/aiprocure/AiProcureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aihackathon.aiprocure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProcureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
